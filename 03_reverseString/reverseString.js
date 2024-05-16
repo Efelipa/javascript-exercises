87b671ade461246dfb73702b2b22bdb43f34d7f3
Erick Felipa <erickfelipa@gmail.com>
2024-05-16 13:14:11 -0500

    Exercise 3: Reverse String - Test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const STRING_MIN = string.trim();
+    const REVERSED_STRING = STRING_MIN.split('').reverse().join('');
+    return REVERSED_STRING;
 };
 
 // Do not edit below this line
