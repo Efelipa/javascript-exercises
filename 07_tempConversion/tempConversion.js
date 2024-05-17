6009b3517f51e810b0e945c4fee3fc4c92b305ee
Erick Felipa <erickfelipa@gmail.com>
2024-05-17 10:13:53 -0500

    Exercise 7: Temperature Converter - Test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let formulaToConvert = (fahrenheit - 32) * (5/9);
+  let toSetToDecimal = Number(formulaToConvert.toFixed(1));
+  return toSetToDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let formulaToConvert = (celsius *(9/5)) + 32;
+  let toSetToDecimal = Number(formulaToConvert.toFixed(1));
+  return toSetToDecimal;
 };
 
 // Do not edit below this line
