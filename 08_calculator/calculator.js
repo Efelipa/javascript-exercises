684e7b6051d48c1d8ec7046414f8c88fe4c1f664
Erick Felipa <erickfelipa@gmail.com>
2024-06-10 14:11:22 -0500

    Exercise 8: Calculator, done!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1 = 0, num2 = 0) {
+	if(typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 = 0, num2 = 0) {
+	if(typeof num1 === 'number' && typeof num2 === 'number') {
+    if(num1 > num2) return num1 - num2;
+    if(num1 < num2) return num2 - num1;
+  }
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((toMultiply, curr) => toMultiply * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(num1 = 0, num2 = 0) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr = new Array();
+  for (let i = 0; i < num; i++) {
+    arr.push(i + 1);
+  }
+  return multiply(arr);
 };
 
 // Do not edit below this line
