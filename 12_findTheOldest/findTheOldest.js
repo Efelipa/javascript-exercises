8140b6c2ece2c74eff8bdd59e96dd6c40c73b754
Erick Felipa <erickfelipa@gmail.com>
2024-06-13 10:27:08 -0500

    Exercise 12: Find the oldest, done!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+    const oldestPerson = people.sort((a, b) => {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
